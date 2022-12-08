7a0d095e454a2eb83fab8305a588f0a23a9c6d81
Samuel Nitzsche <samuel.nitzsche@volkswagen.de>
2022-12-08 16:48:46 +0100

    Clean code

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 var zeiten = {
-    java : [
-            ["10:00", "10:15"],
-            ["12:00", "12:45"], 
-            ["14:30", "14:45"]
-        ],
-    python : [
-            ["10:00", "10:15"],
-            ["11:45", "12:30"], 
-            ["14:00", "14:15"]
-        ],
-    embedded : [
-            ["10:00", "10:15"],
-            ["12:15", "13:00"], 
-            ["14:30", "14:45"]
-        ] 
+    java: [
+        ["10:00", "10:15"],
+        ["12:00", "12:45"],
+        ["14:30", "14:45"]
+    ],
+    python: [
+        ["10:00", "10:15"],
+        ["11:45", "12:30"],
+        ["14:00", "14:15"]
+    ],
+    embedded: [
+        ["10:00", "10:15"],
+        ["12:15", "13:00"],
+        ["14:30", "14:45"]
+    ]
 }
 
 initializeComponents();
 
 var currentTime = new Date();
 var isPause = false;
-var countDownDate = getNextTime(zeiten[localStorage.getItem("group")]);
+var countDown = getNextTime(zeiten[localStorage.getItem("group")]);
 
 function initializeComponents() {
     if (localStorage.getItem('group') == undefined) {
@@ -28,14 +28,14 @@ function initializeComponents() {
     }
 }
 
-function changeGroup(groupname){
+function changeGroup(groupname) {
     localStorage.setItem("group", groupname);
-    countDownDate = getNextTime(zeiten[localStorage.getItem("group")]);
+    countDown = getNextTime(zeiten[localStorage.getItem("group")]);
     document.getElementById("group").innerHTML = groupname;
 }
 
 // function that converts a hour and minute as a string to unix time 
-function convertStringToDate(time){
+function convertStringToDate(time) {
     var splittedTime = time.split(':')
     var givenDate = new Date();
     givenDate.setHours(splittedTime[0]);
@@ -44,42 +44,48 @@ function convertStringToDate(time){
     return givenDate.getTime();
 }
 
-function getNextTime(timesType){
-
-    if(convertStringToDate(timesType[2][1]) < currentTime.getTime()){
-        isPause = false;
-        return convertStringToDate("16:15");
-}
-    for (let i = 0; i < timesType.length; i++) {
-        if(currentTime.getTime() <= convertStringToDate(timesType[i][0])){
-            isPause = false;
-            return convertStringToDate(timesType[i][0]);
-        } else if(currentTime.getTime() <= convertStringToDate(timesType[i][1])){
-            isPause = true;
-            return convertStringToDate(timesType[i][1]); 
+function getNextTime(timesType) {
+    if (convertStringToDate(timesType[2][1]) < currentTime.getTime()) {
+        return {
+            time: convertStringToDate("16:15"),
+            isPause: false
         }
     }
-    
-
+    for (let i = 0; i < timesType.length; i++) {
+        if (currentTime.getTime() <= convertStringToDate(timesType[i][0])) {
+            return {
+                time: convertStringToDate(timesType[i][0]),
+                isPause: false
+            }
+        } else if (currentTime.getTime() <= convertStringToDate(timesType[i][1])) {
+            return {
+                time: convertStringToDate(timesType[i][1]),
+                isPause: true
+            }
+        }
+    }
 }
 
 // Update the count down every 1 second
-var countDownInterval = setInterval(function() {
+var countDownInterval = setInterval(function () {
 
     currentTime = new Date();
 
-    if(convertStringToDate("8:30") > currentTime.getTime()){
-        document.getElementById("isPause").style.display = "unset"; 
+    if (convertStringToDate("8:30") > currentTime.getTime()) {
+        document.getElementById("isPause").style.display = "unset";
         document.getElementById("isPause").innerHTML = "Arbeit beginnt in:";
-        countDownDate = convertStringToDate("8:30");
-    } else if(isPause){
+        countDown = { 
+            time: convertStringToDate("8:30"), 
+            isPause: false
+        };
+    } else if (isPause) {
         document.getElementById("isPause").innerHTML = "Pausenzeit:";
-    }else{
-        document.getElementById("isPause").innerHTML = "Arbeitszeit:"; 
+    } else {
+        document.getElementById("isPause").innerHTML = "Arbeitszeit:";
     }
 
     // Find the distance between the currentTime and the count down date
-    var distance = countDownDate - currentTime.getTime();
+    var distance = countDown.time - currentTime.getTime();
 
     // Time calculations for days, hours, minutes and seconds
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -88,26 +94,24 @@ var countDownInterval = setInterval(function() {
 
     // Display the result in the element with id="timer"
     document.getElementById("timer").innerHTML = hours + "h "
-    + minutes + "m " + seconds + "s ";
+        + minutes + "m " + seconds + "s ";
 
 
     //Check if its weekend
-    if(currentTime.getDay == 5 || currentTime.GetDay == 6){
+    if (currentTime.getDay == 5 || currentTime.GetDay == 6) {
         document.getElementById("timer").innerHTML = "Schönes Wochenende!";
         document.getElementById("isPause").style.display = "none";
         document.getElementById("group").style.display = "none";
     }
 
     //Check if work time is over
-    if(convertStringToDate("16:15") < currentTime.getTime()){
+    if (convertStringToDate("16:15") < currentTime.getTime()) {
         document.getElementById("timer").innerHTML = "Schönen Feierabend!";
         document.getElementById("isPause").style.display = "none";
         document.getElementById("group").style.display = "none";
     }
 
-    
-
     if (distance < 0) {
-        countDownDate = getNextTime(zeiten[localStorage.getItem("group")]);
+        countDown = getNextTime(zeiten[localStorage.getItem("group")]);
     }
 }, 1000);
